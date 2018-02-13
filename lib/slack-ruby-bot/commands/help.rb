0c9309be11275ac0150d5d91cf00ba98656251b3
Eric <emcconkie@grio.com>
2018-02-13 04:56:07 -0800

    remove reference to repo in help

--- lib/slack-ruby-bot/commands/help.rb
+++ lib/slack-ruby-bot/commands/help.rb
@@ -32,7 +32,6 @@ module SlackRubyBot
 
 For getting description of the command use: *help <command>*
 
-For more information see https://github.com/slack-ruby/slack-ruby-bot, please.
 TEXT
         end
       end
